2b20ccdcb228e1463fb932e1c5f0a9119bfedf09
HybridAvenger69 <hybridavenger69@yahoo.com>
2025-02-03 14:11:23 -0700

    ItemRegistry start

--- src/main/java/com/hybridavenger/hybridlib/HybridLib.java
+++ src/main/java/com/hybridavenger/hybridlib/HybridLib.java
@@ -1,5 +1,6 @@
 package com.hybridavenger.hybridlib;
 
+import com.hybridavenger.hybridlib.item.ItemRegistry;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -57,6 +58,8 @@ public class HybridLib
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ItemRegistry.ITEMS.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
---
+++ src/main/java/com/hybridavenger/hybridlib/item/ItemRegistry.java
@@ -0,0 +1,28 @@
+package com.hybridavenger.hybridlib.item;
+
+import com.hybridavenger.hybridlib.HybridLib;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ItemRegistry {
+
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(HybridLib.MOD_ID);
+
+    //Ingots and Gems
+    public static final DeferredItem<Item> TECH_INGOT = ITEMS.register("tech_ingot",
+            () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> EUCLASE_GEM = ITEMS.register("euclase_gem",
+            () -> new Item(new Item.Properties()));
+
+
+
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+}
